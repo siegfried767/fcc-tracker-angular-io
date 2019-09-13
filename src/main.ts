22b0f0908c9a312ad6e8a17cc19122fa1298308b
Siegfried Guentensperger <siegfried767@gmail.com>
2019-09-13 14:56:28 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+/** The below allows Production Mode to be entered on device*/
 import { enableProdMode } from '@angular/core';
+
+/** imports platform-browser-dynamic web applications module*/
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+/** Initiates App interface module and runtime environment*/
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/** Ensures that prouction mode is either entered or that the error will be caught by the console */
 if (environment.production) {
   enableProdMode();
 }
